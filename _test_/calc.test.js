2597dedd8a9921b0308d91ff695c8b2280a8b154
MartinMSalas <ingmartinmsalas@gmail.com>
2022-09-07 21:06:08 -0300

    adding tests

--- _test_/calc.test.js
+++ _test_/calc.test.js
@@ -1,13 +1,99 @@
 //const { default: test } = require('node:test')
-const { suma,resta,multiplicacion,division } = require('../calc.js')
+const { suma,resta,multiplicacion,division, potencia } = require('../calc.js')
 
-test('validar suma correcta de dos numeros', () => {
-    const resultado = suma(10,30);
-    expect(resultado).toBe(40);
-});
+describe("Pruebas de la funcion suma", () => {
+    test('chequear q ambos sean numeros', () => {
+        let resultado = suma("A",30);
+        expect(resultado).toBe(0);
+        resultado = suma(30,"A");
+        expect(resultado).toBe(0);
+        resultado = suma(null,10);
+        expect(resultado).toBe(0);
+        resultado = suma(10,null);
+        expect(resultado).toBe(0);
+        resultado = suma(undefined,10);
+        expect(resultado).toBe(0);
+        resultado = suma(10,undefined);
+        expect(resultado).toBe(0);
+    });
+    
+    test('validar suma correcta de dos numeros', () => {
+        const resultado = suma(10,30);
+        expect(resultado).toBe(40);
+    });
+    
+    
+})
+describe("Pruebas de la funcion resta",() =>{
 
+    test('chequear q ambos sean numeros', () => {
+        let resultado = resta("A",30);
+        expect(resultado).toBe(0);
+        resultado = resta(30,"A");
+        expect(resultado).toBe(0);
+        resultado = resta(null,10);
+        expect(resultado).toBe(0);
+        resultado = resta(10,null);
+        expect(resultado).toBe(0);
+        resultado = resta(undefined,10);
+        expect(resultado).toBe(0);
+        resultado = resta(10,undefined);
+        expect(resultado).toBe(0);
 
-test('chequear q ambos sean numeros', () => {
-    const resultado = suma("A",30);
-    expect(resultado).toBe('Hay un error en la entrada de datos no son numeros');
-});
+    });
+    test('validar resta correcta de dos numeros', () => {
+        const resultado = resta(20,10);
+        expect(resultado).toBe(10);
+    });
+    
+    
+})
+describe("Pruebas de la funcion multiplicacion",() =>{
+
+    test('chequear q ambos sean numeros', () => {
+        let resultado = multiplicacion("A",30);
+        expect(resultado).toBe(0);
+        resultado = multiplicacion(30,"A");
+        expect(resultado).toBe(0);
+        resultado = multiplicacion(null,10);
+        expect(resultado).toBe(0);
+        resultado = multiplicacion(10,null);
+        expect(resultado).toBe(0);
+        resultado = multiplicacion(undefined,10);
+        expect(resultado).toBe(0);
+        resultado = multiplicacion(10,undefined);
+        expect(resultado).toBe(0);
+
+    });
+    test('validar multiplicacion correcta de dos numeros', () => {
+        const resultado = multiplicacion(20,10);
+        expect(resultado).toBe(200);
+    });
+
+    
+    
+})
+describe("Pruebas de la funcion division",() =>{
+
+    test('chequear q ambos sean numeros', () => {
+        let resultado = division("A",30);
+        expect(resultado).toBe(0);
+        resultado = division(30,"A");
+        expect(resultado).toBe(0);
+        resultado = division(null,10);
+        expect(resultado).toBe(0);
+        resultado = division(10,null);
+        expect(resultado).toBe(0);
+        resultado = division(undefined,10);
+        expect(resultado).toBe(0);
+        resultado = division(10,undefined);
+        expect(resultado).toBe(0);
+
+    });
+    test('validar division correcta de dos numeros', () => {
+        const resultado = division(20,10);
+        expect(resultado).toBe(2);
+    });
+    
+    
+})
--- calc.js
+++ calc.js
@@ -1,23 +1,33 @@
 function suma(a,b){
+    
+    if (!(typeof a == 'number')||!(typeof b == 'number')||(a===null || b === null))
+        return 0;
     return a+b;
 }
 
 function resta(a,b){
-    return a*b;
+    if (!(typeof a == 'number')||!(typeof b == 'number')||(a===null || b === null))
+        return 0;
+    return a-b;
 }
 
 function multiplicacion(a,b){
+    if (!(typeof a == 'number')||!(typeof b == 'number')||(a===null || b === null))
+        return 0;
     return a*b;
 }
 
 function division(a,b){
-    return a*b;
+    if (!(typeof a == 'number')||!(typeof b == 'number')||(a===null || b === null))
+        return 0;
+    return a/b;
 }
 
 function potencia(a,b){
+    if (!(typeof a == 'number')||!(typeof b == 'number')||(a===null || b === null))
+        return 0;
     return a^b;
 }
-
 
 
 module.exports = {suma, resta, multiplicacion, division, potencia};
